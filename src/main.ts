87f65b9f2fbf6ddba13cc9ab143e52230f86d712
viviane santos <vjsantos.viviane@gmail.com>
2025-07-29 21:59:10 -0300

    chore: setup NestJS project structure

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  process.env.TZ = '-03:00';
+  
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
