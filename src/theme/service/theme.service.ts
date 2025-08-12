d228d8eb8ab9a74a430eea6303655a0a438be3f8
viviane santos <vjsantos.viviane@gmail.com>
2025-08-12 12:18:43 -0300

    refactor(theme): refactor method delete

--- src/theme/service/theme.service.ts
+++ src/theme/service/theme.service.ts
@@ -1,6 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { ILike, Repository } from 'typeorm';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
+import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Theme } from '../entities/theme.entity';
 
 
@@ -13,8 +13,8 @@ export class ThemeService {
     ) { }
 
     async findAll(): Promise<Theme[]> {
-        
-        const themeList =  await this.themeRepository.find({
+
+        const themeList = await this.themeRepository.find({
             relations: {
                 posts: true,
             }
@@ -25,68 +25,73 @@ export class ThemeService {
         return themeList;
     }
 
-    async findById(id: number): Promise < Theme > {
-    let theme = await this.themeRepository.findOne({
-        where: {
-            id
-        },
-        relations: {
-            posts: true
-        }
-    });
+    async findById(id: number): Promise<Theme> {
+        let theme = await this.themeRepository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                posts: true
+            }
+        });
 
-    if(!theme)
+        if (!theme)
             throw new HttpException(
-        `O tema com ID: ${id} não foi encontrado!`, HttpStatus.NOT_FOUND
-    );
+                `O tema com ID: ${id} não foi encontrado!`, HttpStatus.NOT_FOUND
+            );
 
-    return theme;
-}
+        return theme;
+    }
 
-    async findByContent(content: string): Promise < Theme[] > {
-    const themeList =await this.themeRepository.find({
-        where: {
-            contentTheme: ILike(`%${content}%`)
-        },
-        relations: {
-            posts: true
-        }
-    });
+    async findByContent(content: string): Promise<Theme[]> {
+        const themeList = await this.themeRepository.find({
+            where: {
+                contentTheme: ILike(`%${content}%`)
+            },
+            relations: {
+                posts: true
+            }
+        });
         if (themeList.length === 0)
-      throw new HttpException("Nenhum tema encontrado.", HttpStatus.NOT_FOUND);
+            throw new HttpException("Nenhum tema encontrado.", HttpStatus.NOT_FOUND);
 
-    return themeList;
-}
-
-    async create(theme: Theme): Promise < Theme > {
-    if(theme.id) {
-    const existsTheme = await this.themeRepository.findOne({
-        where: { id: theme.id }
-    });
-    if (existsTheme) {
-        throw new HttpException(
-            `Tema com ID: ${theme.id} já existe!`,
-            HttpStatus.CONFLICT
-        );
-    }
-}
-return await this.themeRepository.save(theme);
+        return themeList;
     }
 
-    async update(theme: Theme): Promise < Theme > {
-    if(!theme.id) {
-    throw new HttpException(
-        'ID do tema é obrigatório para atualização!',
-        HttpStatus.BAD_REQUEST
-    );
-}
-await this.findById(theme.id);
-
-return await this.themeRepository.save(theme);
+    async create(theme: Theme): Promise<Theme> {
+        if (theme.id) {
+            const existsTheme = await this.themeRepository.findOne({
+                where: { id: theme.id }
+            });
+            if (existsTheme) {
+                throw new HttpException(
+                    `Tema com ID: ${theme.id} já existe!`,
+                    HttpStatus.CONFLICT
+                );
+            }
+        }
+        return await this.themeRepository.save(theme);
     }
-    async delete (id: number): Promise < Theme > {
-    const theme = await this.findById(id);
-    await this.themeRepository.delete(id);
-    return theme;
-}
+
+    async update(theme: Theme): Promise<Theme> {
+        if (!theme.id) {
+            throw new HttpException(
+                'ID do tema é obrigatório para atualização!',
+                HttpStatus.BAD_REQUEST
+            );
+        }
+        await this.findById(theme.id);
+
+        return await this.themeRepository.save(theme);
+    }
+
+
+    async delete(id: number): Promise<DeleteResult> {
+        const theme = await this.themeRepository.findOne({ where: { id } });
+        if (!theme) {
+            throw new NotFoundException(`Categoria com ID: ${id} não foi encontrada!`);
+        }
+        return await this.themeRepository.delete(id);
+    }
+
 }
